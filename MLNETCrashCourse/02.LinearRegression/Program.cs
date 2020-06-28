afc8369fd95fa737bc4c3dab39d12a8b01872e31
Pratik Bhattacharya <patrickb404@outlook.com>
2020-06-28 23:22:58 +0530

    Added Linear Regression

--- MLNETCrashCourse/02.LinearRegression/Program.cs
+++ MLNETCrashCourse/02.LinearRegression/Program.cs
@@ -2,35 +2,92 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.Data.DataView;
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using Microsoft.ML.Trainers.FastTree;
+using Microsoft.ML.Trainers;
 using myMLNET.Common.Models;
 using myMLNET.Common.Utils;
+using static Microsoft.ML.Data.DataDebuggerPreview;
 
 namespace myMLNET
 {
     class LinearRegression
     {
-        private static readonly string TrainDataPath = Path.Combine(Environment.CurrentDirectory,"data", "chocolate-data.txt");
-        
+        private static readonly string TrainDataPath = Path.Combine(Environment.CurrentDirectory, "data", "chocolate-data.txt");
+        private const string CocoaPercent = "CocoaPercent";
+
         static void Main(string[] args)
         {
-            /* Add ML Context */
+            var mlContext = new MLContext();
 
-            /* Create TextLoader */
+            // Creates a loader which will help format the data
+            var reader = mlContext.Data.CreateTextLoader(
+                columns: new TextLoader.Column[]
+                {
+                    new TextLoader.Column(CocoaPercent, DataKind.Single, 1),
+                    new TextLoader.Column("Label", DataKind.Single, 4)
+                },
+                hasHeader: true // First line of the data file is a header and not data row
+            );
 
-            /* Print out data */
+            IDataView trainingData = reader.Load(TrainDataPath);
+            Preview(trainingData);
 
-            /* Create pipeline */
+            // Creates a pipeline - All operations like data operations, transformation, training are bundled as a pipeline
+            var pipeline =
+                mlContext.Transforms.Concatenate(outputColumnName: "Features", inputColumnNames: CocoaPercent)
+                .Append(mlContext.Regression.Trainers.PoissonRegression());
 
-            /* Train the model */
+            // Train the model
+            TransformerChain<RegressionPredictionTransformer<PoissonRegressionModelParameters>> model = pipeline.Fit(trainingData);
 
-            /* Get the prediction */
+            // Using the training for one-time prediction
+            var predictionEngine = model.CreatePredictionEngine<ChocolateInput, ChocolateOutput>(mlContext);
 
-            /* Generate graph */
+            // Get the prediction
+            ChocolateOutput prediction = predictionEngine.Predict(new ChocolateInput() { CocoaPercent = 100 });
+
+            Console.WriteLine($"Predicted happiness: {prediction.CustomerHappiness}");
+
+            ChartGeneratorUtil.PlotRegressionChart(new PlotChartGeneratorModel()
+            {
+                Title = "Chocolate Consumer Happiness Prediction",
+                LabelX = "Cocoa Percent",
+                LabelY = "Customer Happiness",
+                ImageName = "CocoaPercentToHappiness.png",
+                PointsList = new List<PlotChartPointsList>
+                {
+                    new PlotChartPointsList {Points = ChartGeneratorUtil.GetChartPointsFromFile(TrainDataPath, 1, 4).ToList()}
+                },
+                MaxLimitY = ChartGeneratorUtil.GetMaxColumnValueFromFile(TrainDataPath, 4) + 10
+            });
 
             Console.ReadKey();
+        }
+
+        static void Preview(IDataView traningData, int maxRows = 10)
+        {
+            var preview = traningData.Preview(maxRows);
+            Console.WriteLine("**************************************");
+            Console.WriteLine($"Loaded training data: {preview}");
+            Console.WriteLine("**************************************");
+
+            foreach (ColumnInfo columnInfo in preview.ColumnView)
+            {
+                Console.Write($"{columnInfo.Column.Name} ");
+            }
+            Console.WriteLine();
+
+            foreach (RowInfo rowInfo in preview.RowView)
+            {
+                foreach (KeyValuePair<string, object> row in rowInfo.Values)
+                {
+                    Console.Write($"{row.Value} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("**************************************");
         }
 
         public class ChocolateInput
